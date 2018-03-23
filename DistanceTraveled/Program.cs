da89193e7af0c4a0349cc0f3d70ad0121677962b
Sam Egger <segger87@gmail.com>
2018-03-23 23:30:15 +0000

    Distance Traveled

---
+++ DistanceTraveled/Program.cs
@@ -0,0 +1,27 @@
+﻿//Find the distance between 2 points
+
+using System;
+
+namespace Distance
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            double time, acceleration, distance;
+
+            Console.WriteLine("Enter Accelaration ");
+            acceleration = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Distance traveled: ");
+            distance = double.Parse(Console.ReadLine());
+
+            time = Math.Sqrt(distance * 2 / acceleration);
+            Console.WriteLine("Your car traveled for " + time + " seconds");
+
+            Console.ReadLine();
+
+        }
+    }
+}
