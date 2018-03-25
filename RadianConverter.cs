2d55bcefc15cb3161c034f0bbd1068caedd0db4e
Sam Egger <segger87@gmail.com>
2018-03-25 16:56:17 +0100

    Radian to Degrees converter

---
+++ RadianConverter.cs
@@ -0,0 +1,18 @@
+﻿//Write a program that gets a user to enter an angle in Radians and converts to degrees
+using System;
+
+namespace RadianToDegrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radian, degree;
+            Console.WriteLine("Enter a Radian number: ");
+            radian = Convert.ToInt32(Console.ReadLine());
+            degree = radian * 180 / Math.PI;
+            Console.WriteLine(radian + " radian is equivalanet to " + degree + " degress");
+            Console.ReadLine();
+        }
+    }
+}
